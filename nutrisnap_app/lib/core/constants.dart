44e7977f817150415034c217cc8d711b26435552
franciscocosta4287 <francisco.costa4287@gmail.com>
2026-03-05 11:18:37 -0300

    fix: add v2 constants.dart

--- nutrisnap_app/lib/core/constants.dart
+++ nutrisnap_app/lib/core/constants.dart
@@ -1,5 +1,8 @@
 class AppConstants {
   // static const String baseUrl = "http://10.0.2.2"; 
-  const String baseUrl = "https://nutrisnap-api.onrender.com";
+  // const String baseUrl = "https://nutrisnap-api-ehqh.onrender.com";
+  static const String baseUrl = "https://nutrisnap-api-ehqh.onrender.com";
+  // const String API_BASE_URL = "https://nutrisnap-api-ehqh.onrender.com";
+  // https://nutrisnap-api-ehqh.onrender.com/api/analyze/
   // Android emulator usa 10.0.2.2 para localhost
 }
